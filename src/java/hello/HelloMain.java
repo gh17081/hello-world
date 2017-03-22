2e41a92240aea0a8ddcc0a21471cba31c2558a5b
gh17081 <gh17081@panulirus.net>
2017-03-22 17:41:39 -0500

    Initial commit

---
+++ src/java/hello/HelloMain.java
@@ -0,0 +1,12 @@
+package hello;
+
+import java.io.PrintStream;
+
+public class HelloMain {
+	private static PrintStream out = System.out;
+
+	public static void main(String[] args) {
+		out.printf("Hello, world!%n");
+	}
+
+}
